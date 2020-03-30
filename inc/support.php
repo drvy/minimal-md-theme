bb8dee41a367024d574ef1a0cefe2372adab64a7
Dragomir Yordanov <bad.stupid.monkey@gmail.com>
2020-03-30 18:45:00 +0100

    Remove theme_support functionality

--- inc/support.php
+++ inc/support.php
@@ -12,33 +12,6 @@
 
 defined( 'ABSPATH' ) || exit;
 
-if ( ! function_exists( 'mmt_after_setup_theme' ) ) {
-	/**
-	 * Indicate theme support.
-	 *
-	 * @since 1.0.0
-	 *
-	 * @return void
-	 */
-	function mmt_add_theme_support() {
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'custom-logo' );
-		add_theme_support(
-			'html5',
-			array(
-				'comment-list',
-				'comment-form',
-				'search-form',
-				'gallery',
-				'caption',
-			)
-		);
-	}
-
-	add_action( 'after_setup_theme', 'mmt_add_theme_support' );
-}
-
-
 if ( ! function_exists( 'mmt_register_menus' ) ) {
 	/**
 	 * Register theme supported menus
